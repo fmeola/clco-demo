691dd7621c2fd805d3b224a6a5dbb18822be8100
Franco Román Meola <franco.meola@icloud.com>
2025-11-25 16:45:10 +0100

    Code from Assignment 5 submission

--- main.tf
+++ main.tf
@@ -2,24 +2,137 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "norwayeast"
+# Resource Group
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+
+# App Service Plan
+
+resource "azurerm_service_plan" "appplan" {
+  name                = "${var.prefix}-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
-resource "azurerm_linux_web_app" "example" {
-  name                = "clcog6webapp"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+
+# Virtual Network + Subnets
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet for Web App
+resource "azurerm_subnet" "web_subnet" {
+  name                 = "${var.prefix}-web-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "webapp-delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+# Subnet for Private Endpoint
+resource "azurerm_subnet" "pe_subnet" {
+  name                 = "${var.prefix}-pe-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+
+# Cognitive Account
+
+resource "azurerm_cognitive_account" "cog" {
+  name                  = "${var.prefix}-cog"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  kind                  = "TextAnalytics"
+  sku_name              = "S"
+  custom_subdomain_name = "${var.prefix}cogservice"
+
+  public_network_access_enabled = false
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
+}
+
+resource "azurerm_user_assigned_identity" "webapp_identity" {
+  name                = "${var.prefix}-mi"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+
+# Private DNS Zone
+resource "azurerm_private_dns_zone" "dns" {
+  name                = "privatelink.cognitiveservices.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dnslink" {
+  name                  = "${var.prefix}-dns-link"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+
+# Private Endpoint
+resource "azurerm_private_endpoint" "pe" {
+  name                = "${var.prefix}-pe"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.pe_subnet.id
+
+  private_service_connection {
+    name                           = "${var.prefix}-pe-connection"
+    private_connection_resource_id = azurerm_cognitive_account.cog.id
+    subresource_names              = ["account"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "${var.prefix}-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns.id]
+  }
+}
+
+
+# Web App
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "${var.prefix}-webapp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.appplan.location
+  service_plan_id     = azurerm_service_plan.appplan.id
+
+  https_only = true
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.webapp_identity.id
+    ]
+  }
+
+  virtual_network_subnet_id = azurerm_subnet.web_subnet.id
 
   site_config {
     application_stack {
@@ -29,90 +142,7 @@ resource "azurerm_linux_web_app" "example" {
   }
 
   app_settings = {
-    "AZ_ENDPOINT" = azurerm_cognitive_account.example.endpoint
-    "AZ_KEY"      = azurerm_cognitive_account.example.primary_access_key
+    "AZ_ENDPOINT" = "https://${azurerm_cognitive_account.cog.custom_subdomain_name}.cognitiveservices.azure.com/"
+    "AZ_KEY"      = azurerm_cognitive_account.cog.primary_access_key
   }
-}
-
-resource "azurerm_app_service_virtual_network_swift_connection" "example" {
-  app_service_id = azurerm_linux_web_app.example.id
-  subnet_id      = azurerm_subnet.example.id
-}
-
-resource "azurerm_cognitive_account" "example" {
-  name                = "example-account"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  kind                = "TextAnalytics"
-  sku_name            = "S"
-  custom_subdomain_name = "clcog62025csdn" 
-  public_network_access_enabled = false
-  network_acls {
-    default_action = "Deny"
-    bypass         = "AzureServices"
-  }
-  tags = {
-    Acceptance = "Test"
-  }
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
-}
-
-resource "azurerm_subnet" "pe_subnet" {
-  name                 = "pe-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_private_endpoint" "example" {
-  name                = "example-endpoint"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  subnet_id           = azurerm_subnet.pe_subnet.id
-
-  private_service_connection {
-    name                           = "example-privateserviceconnection"
-    private_connection_resource_id = azurerm_cognitive_account.example.id
-    subresource_names              = ["account"]
-    is_manual_connection           = false
-  }
-
-  private_dns_zone_group {
-    name                 = "cognitive-dns-zone-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.example.id]
-  }
-}
-
-resource "azurerm_private_dns_zone" "example" {
-  name                = "privatelink.cognitiveservices.azure.com"
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "example" {
-  name                  = "example-link"
-  resource_group_name   = azurerm_resource_group.example.name
-  private_dns_zone_name = azurerm_private_dns_zone.example.name
-  virtual_network_id    = azurerm_virtual_network.example.id
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  description = "Name prefix for all resources"
+  type        = string
+  default     = "clcogroup6fmeola"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "norwayeast"
+}
